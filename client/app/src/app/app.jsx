9490a766c88c2f1f6669ee18d46233470505fb5a
paullasarev <paullasarev@gmail.com>
2019-02-10 15:47:41 +0400

    implement dnd resize

--- client/app/src/app/app.jsx
+++ client/app/src/app/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import HTML5Backend from 'react-dnd-html5-backend'
+import { DragDropContext } from 'react-dnd'
 
 import configureStore from './configure-store';
 import Edit from '../components/edit/edit.jsx';
@@ -20,5 +22,5 @@ const App = () => {
   );
 };
 
-export default App;
+export default DragDropContext(HTML5Backend)(App);
 
--- client/app/src/controls/tabs/tabs.jsx
+++ client/app/src/controls/tabs/tabs.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { find, map } from 'lodash';
 import classNames from 'classnames';
+import { DragSource } from 'react-dnd';
 
 import './tabs.scss';
+
+const MIN_WIDTH = 200;
 
 const TabHeader = ({name, isActive, setActive}) => {
   return (
@@ -17,22 +20,99 @@ const TabHeader = ({name, isActive, setActive}) => {
   )
 }
 
-export const Tabs = (props) => {
-  const { children, active, setActive } = props;
-  const activeTab = find(children, { props: {name: active}});
-  const headers = map(children, ({props:{name}}) => (
-    <TabHeader {...{name, isActive: name === active, key:name, setActive}} />
-  ))
-  return (
-    <div className='tabs'>
-      <div className='tabs__header'>
-        {headers}
-      </div>
-      <div className='tabs__tab'>
-        { activeTab }
-      </div>
-    </div>
-  );
+function collect(connect, monitor) {
+  return {
+    // Call this function inside render()
+    // to let React DnD handle the drag events:
+    connectDragSource: connect.dragSource(),
+    // You can ask the monitor about the current drag state:
+    isDragging: monitor.isDragging()
+  };
 }
 
-export default Tabs;
+/**
+ * Specifies the drag source contract.
+ * Only `beginDrag` function is required.
+ */
+const tabsSource = {
+  beginDrag(props, monitor, component) {
+    // Return the data describing the dragged item
+    console.log('beginDrag', monitor.getClientOffset(), component)
+    const { x: xStart } = monitor.getClientOffset();
+    const { getNode } = component;
+    const item = { getNode, xStart };
+    return item;
+  },
+
+  // canDrag(props, monitor) {
+  //   return true;
+  // },
+
+  // isDragging(props, monitor) {
+  //   console.log('isDragging', monitor.getClientOffset())
+  //   return true;
+  // },
+
+  endDrag(props, monitor, component) {
+    const item = monitor.getItem();
+    const { getNode, xStart } = item;
+    const node = getNode && getNode();
+    if (!node) {
+      return;
+    }
+    const { x: xEnd } = monitor.getClientOffset();
+    const dx = xStart - xEnd;
+    const { width } = node.getBoundingClientRect();
+    const newWidth = Math.max(width + dx, MIN_WIDTH);
+
+    console.log('endDrag', dx, width, newWidth)
+    node.style.width = `${newWidth}px`;
+
+    // if (!monitor.didDrop()) {
+    //   return;
+    // }
+
+    // When dropped on a compatible target, do something
+    // const dropResult = monitor.getDropResult();
+    // CardActions.moveCardToList(item.id, dropResult.listId);
+  },
+};
+
+export class Tabs extends PureComponent {
+  getNode = () => {
+    return this.tabsNode;
+  }
+
+  render() {
+    const props = this.props;
+    const { children, active, setActive } = props;
+
+    const activeTab = find(children, { props: {name: active}});
+    const headers = map(children, ({props:{name}}) => (
+      <TabHeader {...{name, isActive: name === active, key:name, setActive}} />
+    ))
+
+    const { isDragging, connectDragSource } = props;
+
+    return (
+      <div
+        className='tabs'
+        ref={ (node) => { this.tabsNode = node; } }
+      >
+        <div className='tabs__header'>
+          {headers}
+        </div>
+        <div className='tabs__tab'>
+          { activeTab }
+        </div>
+        { connectDragSource(<div
+          className={classNames('tabs__resizer',{
+            'tabs__resizer--dragging': isDragging
+          })}
+        />) }
+      </div>
+    );
+  }
+}
+
+export default DragSource('tabs', tabsSource, collect)(Tabs);
